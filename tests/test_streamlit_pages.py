7ef38546c47ecf98c58f67b6240664be5a547e15
Zachary Blackwood <zachary@streamlit.io>
2022-11-14 17:32:16 -0500

    Add testing, linting, gh action s, move folder

--- tests/test_streamlit_pages.py
+++ tests/test_streamlit_pages.py
@@ -1,9 +1,9 @@
 def test_import():
-    from streamlit_pages import Page, show_pages
+    from st_pages import Page, show_pages  # noqa: F401
 
 
 def test_page():
-    from streamlit_pages import Page
+    from st_pages import Page
 
     page = Page("tests/test_streamlit_pages.py")
     assert page.page_name == "Test Streamlit Pages"
@@ -12,7 +12,7 @@ def test_page():
 
 
 def test_page_icon():
-    from streamlit_pages import Page
+    from st_pages import Page
 
     page = Page("tests/test_streamlit_pages.py", icon=":dog:")
     assert page.page_icon == "🐶"

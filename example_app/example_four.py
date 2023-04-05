5902cc86a5dd2b43fc8b1d4a1e0069aa3dea84ee
Zachary Blackwood <zachary@streamlit.io>
2023-04-05 15:32:54 -0400

    Add usage of hide_pages to example app and add tests

--- example_app/example_four.py
+++ example_app/example_four.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
-from st_pages import add_page_title
+from st_pages import add_page_title, hide_pages
 
 add_page_title()
 
 st.write("This is just a sample page!")
+
+selection = st.radio(
+    "Test page hiding",
+    ["Show all pages", "Hide pages 1 and 2", "Hide Other apps Section"],
+)
+
+if selection == "Show all pages":
+    hide_pages([])
+elif selection == "Hide pages 1 and 2":
+    hide_pages(["Example One", "Example Two"])
+elif selection == "Hide Other apps Section":
+    hide_pages(["Other apps"])

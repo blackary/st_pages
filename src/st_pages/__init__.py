f04b5c4568bced246e4c2ec3b6e320b31a6a3805
Natarajan Sairam <natarajansairam@natarajans-air.home>
2024-08-08 20:36:20 +0100

    explicit icon attr check and cast to Optional[str]

--- src/st_pages/__init__.py
+++ src/st_pages/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import json
 from dataclasses import dataclass
 from pathlib import Path
-
+from typing import cast
 import streamlit as st
 import toml
 from streamlit.commands.page_config import get_random_emoji
@@ -75,10 +75,10 @@ def _get_nav_from_toml(
 
         for page in pages:
             if page.is_section:
-                if translate_icon(page.icon):
+                if page.icon is not None:
                     current_section = f"{translate_icon(page.icon)} {page.name}"
                 else:
-                    current_section = page.name
+                    current_section = cast(str, page.name)
                 if page.name in st.session_state[HIDE_PAGES_KEY]:
                     sections_to_drop.append(current_section)
                 continue
